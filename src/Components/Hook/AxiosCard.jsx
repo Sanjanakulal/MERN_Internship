7d2835a1311dbbe4d260748ae8e351491319b5f4
Sanjanakulal <sanjanakulal2005@gmail.com>
2026-02-25 16:39:32 +0530

    added localstorage functionality

--- src/Components/Hook/AxiosCard.jsx
+++ src/Components/Hook/AxiosCard.jsx
@@ -29,10 +29,10 @@ export default function AxiosCard() {
     useEffect(()=>{
         handlefetch()
     },[])
-   
+
 
   return (
-    
+
     <div style={{ padding: "20px" }}>
       <button onClick={handlefetch}>click</button>
 
@@ -46,7 +46,7 @@ export default function AxiosCard() {
       >
   {data.map((item)=>{
   return(
-    <Card key={item.id} sx={{ width: 300, margin: 2 }}>
+    <Card key={item.id} style={{ width: 300, margin: 2 }}>
 
       <CardHeader
         title={item.title}
@@ -83,6 +83,133 @@ export default function AxiosCard() {
 })}
     </div>
     </div>
-   
+
   )
 }
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react'
+// import axios from 'axios';
+// import Card from "@mui/material/Card";
+// import CardHeader from "@mui/material/CardHeader";
+// import CardMedia from "@mui/material/CardMedia";
+// import CardContent from "@mui/material/CardContent";
+// import CardActions from "@mui/material/CardActions";
+// import Collapse from "@mui/material/Collapse";
+// import IconButton from "@mui/material/IconButton";
+// import Typography from "@mui/material/Typography";
+// import FavoriteIcon from "@mui/icons-material/Favorite";
+// import ShareIcon from "@mui/icons-material/Share";
+// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+// export default function AxiosCard() {
+//   const [data, setData] = useState([]);
+//   const [openMap, setOpenMap] = useState({});
+
+//   const handlefetch = () => {
+//     axios.get('https://dummyjson.com/products')
+//       .then((res) => {
+//         setData(res.data.products);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+
+//   useEffect(() => {
+//     handlefetch();
+//   }, []);
+
+//   // toggle expand per card
+//   const handleExpand = (id) => {
+//     setOpenMap(prev => ({
+//       ...prev,
+//       [id]: !prev[id]
+//     }));
+//   };
+
+//   return (
+//     <div style={{ padding: "20px" }}>
+//       <button onClick={handlefetch}>click</button>
+
+//       <div
+//         style={{
+//           display: "flex",
+//           flexWrap: "wrap",
+//           gap: "20px",
+//           marginTop: "20px",
+//         }}
+//       >
+//         {data.map((item) => (
+//           <Card key={item.id} style={{ width: 300, margin: 2 }}>
+
+//             <CardHeader
+//               title={item.title}
+//               subheader={item.category}
+//             />
+
+//             <CardMedia
+//               component="img"
+//               height="180"
+//               image={item.thumbnail}
+//               alt={item.title}
+//             />
+
+//             <CardContent>
+//               <Typography variant="body2">
+//                 {item.description}
+//               </Typography>
+
+//               <Typography variant="h6">
+//                 ₹{item.price}
+//               </Typography>
+//             </CardContent>
+
+//             <CardActions disableSpacing>
+//               <IconButton>
+//                 <FavoriteIcon />
+//               </IconButton>
+
+//               <IconButton>
+//                 <ShareIcon />
+//               </IconButton>
+
+//               {/* expand button on right */}
+//               <IconButton
+//                 onClick={() => handleExpand(item.id)}
+//                 style={{ marginLeft: "auto" }}
+//               >
+//                 <ExpandMoreIcon />
+//               </IconButton>
+//             </CardActions>
+
+//             {/* expandable real product details */}
+//             <Collapse in={!!openMap[item.id]} timeout="auto" unmountOnExit>
+//               <CardContent>
+//                 <Typography variant="body2">
+//                    Rating: {item.rating}
+//                 </Typography>
+
+//                 <Typography variant="body2">
+//                   Brand: {item.brand}
+//                 </Typography>
+
+//                 <Typography variant="body2">
+//                   Stock: {item.stock}
+//                 </Typography>
+//               </CardContent>
+//             </Collapse>
+
+//           </Card>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
--- src/Components/Mui/Register.jsx
+++ src/Components/Mui/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typography from '@mui/material/Typography'
 import Paper from '@mui/material/Paper'
 import TextField from '@mui/material/TextField'
@@ -6,16 +6,38 @@ import Button from '@mui/material/Button'
 
 
 export default function Register() {
+  const [formdata,setFormdata] = useState({
+    name:'',
+    email:'',
+    password:'',
+    phone:'',
+    address:''
+  })
+  const handlechange =(e)=>{
+    console.log({...formdata, [e.target.name]:e.target.value})
+     setFormdata({...formdata ,[e.target.name]:e.target.value})
+
+  }
+
+  const handleregister =()=>{
+
+    const existingusers = JSON.parse(localStorage.getItem('userdetails')) || [];
+    console.log(existingusers)
+    const allusers =[...existingusers,formdata]
+
+    localStorage.setItem('userdetails',JSON.stringify(allusers))
+    alert("registration done!!")
+  }
   return (
     <div>
       <Paper elevation={20} style={{width:"550px",padding:"20px",margin:"50px auto"}}>
      <Typography variant='h3'>Register Page</Typography>
-     <TextField variant='outlined' type='text' label='Name' fullWidth style={{marginBottom:"10px"}}/>
-     <TextField variant='outlined' type='email' label='Email' fullWidth style={{marginBottom:"10px"}}/>
-     <TextField variant='outlined' type='password' label='Password' fullWidth style={{marginBottom:"10px"}}/>
-     <TextField variant='outlined' type='number'label='Phone' fullWidth style={{marginBottom:"10px"}}/>
-     <TextField variant='outlined' multiline rows={5} label='Address' fullWidth style={{marginBottom:"10px"}}/>
-    <Button variant='contained' fullWidth>Register</Button>
+     <TextField variant='outlined' type='text' label='Name' name='name' fullWidth style={{marginBottom:"10px"}} onChange={handlechange}/>
+     <TextField variant='outlined' type='email' label='Email' name='email' fullWidth style={{marginBottom:"10px"}} onChange={handlechange}/>
+     <TextField variant='outlined' type='password' label='Password' name='password' fullWidth style={{marginBottom:"10px"}} onChange={handlechange}/>
+     <TextField variant='outlined' type='number'label='Phone' name='phone' fullWidth style={{marginBottom:"10px"}} onChange={handlechange}/>
+     <TextField variant='outlined' multiline rows={5} label='Address' name='address' fullWidth style={{marginBottom:"10px"}} onChange={handlechange}/>
+    <Button variant='contained' fullWidth onClick={handleregister}>Register</Button>
      </Paper>
       
     </div>
